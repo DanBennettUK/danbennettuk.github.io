8abe59f814b7530120393c083e2c6883a9a31a5c
Eevee <eevee@clawd.bot>
2026-02-01 08:47:51 +0100

    Fix Vercel build: UTF-8 encoding, remove ruby23 (deprecated) 🦊

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
-yum install ruby23-devel.x86_64 zlib-devel
+#!/bin/bash
+# Build script for Vercel with UTF-8 encoding support
+
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export RUBYOPT="-E utf-8"
+
+yum install -y zlib-devel
 
 gem install jekyll
-
 gem install bundler
-
 gem install json
 
 bundle install
-
 bundle exec jekyll build
